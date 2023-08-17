3c8d5da358c1a5e4d379fd28218ef5acdba2d13d
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-17 22:15:43 +0300

    create common folder and common.js

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
+import { getMonth } from './common/common';
+
 function App() {
+
+  console.table(getMonth());
+
   return (
     <div className="App">
-    
+    Hi
     </div>
   );
 }
---
+++ calendar/src/common/common.js
@@ -0,0 +1,23 @@
+import dayjs from "dayjs";
+
+// 0 - Sunday
+// 6 - Saturday
+
+
+
+//Setting the month to the current month by default
+
+export const getMonth = (month = dayjs().month()) => {
+    const year = dayjs().year();
+    const firstWeekDayOfMonth = dayjs(new Date(year, month, 1)).day();
+    let currentMonthCount = 0 - firstWeekDayOfMonth;
+
+    const daysMatrix = new Array(5).fill([]).map(() => {
+        return new Array(7).fill(null).map(() => {
+            currentMonthCount++;
+            return dayjs(new Date(year, month, currentMonthCount))
+        })
+    });
+     //the first array will have every day of the week
+     return daysMatrix;
+} 
