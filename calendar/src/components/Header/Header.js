5e6a3728efb9030374e627e9eb72f7600d99f119
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-18 13:52:32 +0300

    Create Header component

--- calendar/src/components/Header/Header.js
+++ calendar/src/components/Header/Header.js
@@ -1,7 +1,22 @@
-
+import styles from './Header.module.css';
+import logo from '../../assets/logo.png'
 
 const Header = () => {
-
+    return (
+        <header className={styles['header-calendar']}>
+            <img src={logo} alt="calendar-logo" className={styles['calendar-logo']} />
+            <h1 className={styles['header-headline']}>Calendar</h1>
+            <button className={styles['header-btn']}>
+            Today
+            </button>
+            <button>
+                <span className='material-icons-outlined cursor-pointer text-white'>
+                    chevron_left
+                </span>
+            </button>
+        </header>
+       
+    );
 }
 
 export default Header;
