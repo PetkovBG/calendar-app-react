0001d0860c74c5a0ee0276dfe13452547f70d6c8
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-18 15:08:32 +0300

    Add buttons to change the current month

--- calendar/src/components/Header/Header.js
+++ calendar/src/components/Header/Header.js
@@ -6,16 +6,21 @@ const Header = () => {
         <header className={styles['header-calendar']}>
             <img src={logo} alt="calendar-logo" className={styles['calendar-logo']} />
             <h1 className={styles['header-headline']}>Calendar</h1>
-            <button className={styles['header-btn']}>
-            Today
+            <button className={styles['btn-header']}>
+                Today
             </button>
-            <button>
+            <button className={styles['btn-secondary']}>
                 <span className='material-icons-outlined cursor-pointer text-white'>
                     chevron_left
                 </span>
             </button>
+            <button className={styles['btn-secondary']}>
+                <span className='material-icons-outlined cursor-pointer text-white'>
+                    chevron_right
+                </span>
+            </button>
         </header>
-       
+
     );
 }
 
