911925c1ebe7b1317bd17d975bd4f80183aaf297
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-19 14:42:13 +0300

    Create MiniCalendar component

---
+++ calendar/src/components/MiniCalendar/MiniCalendar.js
@@ -0,0 +1,11 @@
+import styles from './MiniCalendar.module.css';
+
+const MiniCalendar = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    );
+}
+
+export default MiniCalendar;
--- calendar/src/components/SideBar/SideBar.js
+++ calendar/src/components/SideBar/SideBar.js
@@ -1,10 +1,12 @@
 import CreateEvent from '../CreateEvent/CreateEvent';
+import MiniCalendar from '../MiniCalendar/MiniCalendar';
 import styles from './SideBar.module.css';
 
 const SideBar = () => {
     return (
         <aside className={styles['sidebar-aside']}>
             <CreateEvent />
+            <MiniCalendar />
         </aside>
     );
 }
