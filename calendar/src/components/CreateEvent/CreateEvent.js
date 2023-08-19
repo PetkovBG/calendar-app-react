1a9689d5759d100a061353e0f72e93415a08a335
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-19 14:15:01 +0300

    Create CreateEvent component

---
+++ calendar/src/components/CreateEvent/CreateEvent.js
@@ -0,0 +1,11 @@
+import styles from './CreateEvent.module.css';
+
+const CreateEvent = () => {
+    return(
+        <div>
+            CreateEventBtn
+        </div>
+    );
+}
+
+export default CreateEvent;
--- calendar/src/components/SideBar/SideBar.js
+++ calendar/src/components/SideBar/SideBar.js
@@ -1,7 +1,12 @@
-
+import CreateEvent from '../CreateEvent/CreateEvent';
+import styles from './SideBar.module.css';
 
 const SideBar = () => {
-
+    return (
+        <aside className={styles['sidebar-aside']}>
+            <CreateEvent />
+        </aside>
+    );
 }
 
 export default SideBar;
