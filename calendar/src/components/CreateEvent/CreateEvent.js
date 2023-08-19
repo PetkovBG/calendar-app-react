8547f2f988411464081fdb281ebfc3ff0181d6ab
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-19 14:34:03 +0300

    Style create event btn

--- calendar/src/components/CreateEvent/CreateEvent.js
+++ calendar/src/components/CreateEvent/CreateEvent.js
@@ -1,10 +1,12 @@
 import styles from './CreateEvent.module.css';
+import plusSVG from '../../assets/plus.svg'
 
 const CreateEvent = () => {
     return(
-        <div>
-            CreateEventBtn
-        </div>
+        <button className={styles['create-event-btn']}>
+            <img src={plusSVG} alt="create-btn" className={styles['create-event-img']} />
+            <span className={styles['create-event-text']}>Create</span>
+        </button>
     );
 }
 
