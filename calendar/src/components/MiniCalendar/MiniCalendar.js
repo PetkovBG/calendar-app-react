5e4706972047fe7f647efb00de6db7792c535629
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-08-19 15:05:02 +0300

    style MiniCalendar component

--- calendar/src/components/MiniCalendar/MiniCalendar.js
+++ calendar/src/components/MiniCalendar/MiniCalendar.js
@@ -1,9 +1,34 @@
 import styles from './MiniCalendar.module.css';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { getMonth } from '../../common/common';
 
 const MiniCalendar = () => {
+
+    const [currentMonthIndex, setCurrentMonthIndex] = useState(dayjs().month());
+    const [currentMonth, setCurrentMonth] = useState(getMonth());
+
+    useEffect(() => {
+        setCurrentMonth(getMonth(currentMonthIndex))
+    }, [currentMonthIndex])
+
     return (
-        <div>
-            Hi
+        <div className={styles['mini-calendar-wrapper']}>
+            <header className={styles['mini-calendar-header']}>
+            <p className={styles['mini-calendar-text']}>
+                {dayjs(new Date(dayjs().year(), currentMonthIndex)).format("MMMM YYYY")}
+            </p>
+            <button className={styles['arrow']}>
+                <span className='material-icons-outlined arrow-span'>
+        chevron_left
+                </span>
+            </button>
+            <button className={styles['arrow']}>
+                <span className='material-icons-outlined'>
+        chevron_right
+                </span>
+            </button>
+            </header>
         </div>
     );
 }
